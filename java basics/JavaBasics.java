fe245e7dcb775a76d79de2a8348478669a66d544
hema <bhemavallika@gmail.com>
2025-06-16 12:59:28 +0530

    starting DSA

---
+++ java basics/JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics {
+   public static void printHelloWorld(){
+      System.out.println("Hello, World!");
+   }
+
+   public static void main(String[] args) {
+      printHelloWorld();
+   }
+}
